610ae51c28a1b7f6191b4e5079c93773760a0358
azdrojowa248896 <azdrojowa248896@e-science.pl>
2021-10-14 22:55:44 +0200

    feature: add new request mappings

--- backend/csv-validate/src/main/java/com/calc/csvvalidate/CsvValidationController.java
+++ backend/csv-validate/src/main/java/com/calc/csvvalidate/CsvValidationController.java
@@ -1,15 +1,12 @@
 package com.calc.csvvalidate;
-
-import com.calc.survivalcurvedata.dto.request.TestingPerson;
-import com.calc.survivalcurvedata.dto.response.PeriodResultKM;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -22,17 +19,22 @@ public class CsvValidationController {
     @Autowired
     SurvivalCurveClient survivalCurveClient;
 
+    @Autowired
+    private CircuitBreakerFactory circuitBreakerFactory;
+
+
+    @PostMapping(path = "/csv-data/calc/{periods}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity calculateCsvData(@RequestBody Map<Integer, Integer> csvData, @PathVariable(value = "periods") int periods) {
+        CircuitBreaker circuitBreaker = circuitBreakerFactory.create("csv-circuit-breaker");
+        if (validationService.validate(csvData, periods)) {
+            return circuitBreaker.run(() -> survivalCurveClient.getSurvivalCurveResults(validationService.convertData(csvData), periods),
+                    throwable -> ResponseEntity.status(HttpStatus.GATEWAY_TIMEOUT).body("Connection to server responsible for computation of the survival curve is not available"));
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Entered CSV data are not correct, please enter data correctly and validate them before send to calculations");
+    }
+
     @PostMapping(path = "/csv-data/validate/{periods}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity validateData(@RequestBody Map<Integer, Integer> csvData, @PathVariable(value = "periods") int periods) {
-        boolean result = validationService.validate(csvData, periods);
-        List<TestingPerson> temp = new ArrayList<>();
-        temp.add(new TestingPerson(3, true));
-        temp.add(new TestingPerson(4, true));
-        temp.add(new TestingPerson(10, true));
-        temp.add(new TestingPerson(11, false));
-        List<PeriodResultKM> results = survivalCurveClient.getSurvivalCurveResults(temp, 12).getBody();
-        System.out.println(results);
-        System.out.println("results");
-        return new ResponseEntity(result, HttpStatus.OK);
+    public ResponseEntity<Boolean> validateCsvData(@RequestBody Map<Integer, Integer> csvData, @PathVariable(value = "periods") int periods) {
+        return ResponseEntity.status(HttpStatus.OK).body(validationService.validate(csvData, periods));
     }
 }
